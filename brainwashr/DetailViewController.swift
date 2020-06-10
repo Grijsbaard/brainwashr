61e4cc1b4059f135147c759171a377b6cbd6c6fb
Pieter-Jan Delaruelle <pieterjan@culturedtri.be>
2020-06-10 19:58:17 +0200

    Initial Commit

---
+++ brainwashr/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  brainwashr
+//
+//  Created by Pieter-Jan Delaruelle on 10/06/2020.
+//  Copyright © 2020 Pieter-Jan Delaruelle. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
